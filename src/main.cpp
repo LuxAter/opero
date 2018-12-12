8d6d6c4c5b8318085812906a11658df3c5f2bde5
Arden Rasmussen <ardenisthebest@gmail.com>
2018-12-11 16:01:23 -0800

    Old changes

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,7 @@
 
 #include <iostream>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char* argv[]) {
   opero::function<opero::real, opero::real> func(argv[1]);
   std::cout << func(3.1415) << "<<\n";
   return 0;
